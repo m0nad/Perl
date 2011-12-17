e02017f91474440cf90f45ca33f1f5dacc546b0e
Victor Ramos Mello <victornrm@gmail.com>
2011-12-17 15:31:31 -0200

    objdump2shellcode

---
+++ shellcode_gen.pl
@@ -0,0 +1,35 @@
+=c
+objdump2shellcode 
+This uses a binary and objdump to get the opcodes
+parse-it and print the C code with the shellcode.
+ex: http://pastebin.com/ghzfy5sT
+=cut
+#!/usr/bin/perl
+use strict;
+use warnings;
+print "\t$0 by m0nad\n";
+my $file = shift || die "usage:\n  perl $0 ./binary\n";
+my @dump = `objdump -d $file`;
+print "const char sc[] =\n";
+foreach (@dump) {
+  my ($opcode, $instruction) = (split /\t/)[1,2];
+  next unless $opcode;
+  chomp $instruction;
+  print "\""; 
+  my @opcodes = split / /, $opcode;
+  foreach (@opcodes) {
+    next if /^[\s\t]+$/;
+    print "\\x$_";  
+  }
+  print "\"\t//$instruction\n";
+}
+print ";";
+print "
+int
+main()
+{
+  __asm__(\"jmp sc\");
+  return 0;
+}
+";
+
